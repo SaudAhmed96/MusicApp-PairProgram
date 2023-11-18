6d6ae07a2ac993426467408d134540bf642be960
Saud Ahmed <saudahmed96x@gmail.com>
2023-11-18 18:15:22 -0500

    working list of recommendations

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,28 +1,54 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import token from "./data/token.json"
+import token from "./data/token.json";
 // https://api.spotify.com/v1/recommendations/available-genre-seeds
-const BASE_URL = "https://api.spotify.com/v1/"
-const GENRE_END = 'recommendations/available-genre-seeds'
-
-
+const BASE_URL = "https://api.spotify.com/v1/";
+const GENRE_END = "recommendations/available-genre-seeds";
+const rec_End = "recommendations";
+const search_End = "search";
 
 function App() {
   const [genre, setGenre] = useState();
-console.log("token.access_token: ", token.access_token)
+  const [searchItem, setSearchItem] = useState("photograph");
+  // const [songId, setSongId] = useState("");
+  const [choices, setChoices] = useState([]);
+
   function fetchData() {
-    axios.get(`${BASE_URL}${GENRE_END}`, { headers: { Authorization: `Bearer  ${token.access_token}` } })
+    axios
+      .get(`${BASE_URL}${GENRE_END}`, {
+        headers: { Authorization: `Bearer  ${token.access_token}` },
+      })
       .then((res) => {
-        console.log(res)
-        setGenre(res.data)
+        // console.log(res.data.genres);
+        setGenre(res.data.genres);
       });
-    console.log("hello world");
+
+    axios
+      .get(`${BASE_URL}${search_End}?q=${searchItem}&type=track`, {
+        headers: { Authorization: `Bearer  ${token.access_token}` },
+      })
+      .then((res) => {
+        console.log(res.data.tracks.items[0].id);
+
+        fetchRec(res.data.tracks.items[0].id);
+        // setGenre(res.data.tracks.items);
+      });
   }
 
+  function fetchRec(songNum) {
+    axios
+      .get(`${BASE_URL}${rec_End}?seed_tracks=${songNum}`, {
+        headers: { Authorization: `Bearer  ${token.access_token}` },
+      })
+      .then((res) => {
+        console.log(res.data.tracks);
+        setChoices(res.data.tracks);
+      });
+  }
 
   useEffect(() => {
-    fetchData()
+    fetchData();
   }, []);
 
   return (
@@ -39,7 +65,15 @@ console.log("token.access_token: ", token.access_token)
         </div>
         <button className="nav__button">Submit</button>
       </header>
-      <p>Hello Thar!</p>
+      <ul>
+        {choices.map((choice) => {
+          return (
+            <>
+              <li>{choice.name}</li>
+            </>
+          );
+        })}
+      </ul>
     </div>
   );
 }
