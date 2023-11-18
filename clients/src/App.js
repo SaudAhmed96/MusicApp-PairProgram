ce9b830941271c79353658c7add8d76935f70ae0
Bryce Borer <bryce.borer@gmail.com>
2023-11-18 17:26:07 -0500

    first axios get call

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,16 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import token from "./data/token.json"
+// https://api.spotify.com/v1/recommendations/available-genre-seeds
+const BASE_URL = "https://api.spotify.com/v1/"
+const GENRE_END = 'recommendations/available-genre-seeds'
+
+
 
 function App() {
+  const [genre, setGenre] = useState();
+console.log("token.access_token: ", token.access_token)
   function fetchData() {
-    axios.get();
+    axios.get(`${BASE_URL}${GENRE_END}`, { headers: { Authorization: `Bearer  ${token.access_token}` } })
+      .then((res) => {
+        console.log(res)
+        setGenre(res.data)
+      });
     console.log("hello world");
   }
+
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
   return (
     <div className="App">
-      <navbar className="nav">
+      <header className="nav">
         <h1>SongFinder</h1>
         <p>Find similar songs to your favourite song</p>
         <div className="nav__searchbar">
@@ -21,7 +38,8 @@ function App() {
           />
         </div>
         <button className="nav__button">Submit</button>
-      </navbar>
+      </header>
+      <p>Hello Thar!</p>
     </div>
   );
 }
