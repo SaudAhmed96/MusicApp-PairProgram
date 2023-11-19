578fc7f9abbd3515b4ed308e32dbaf7694a5b9c5
Saud Ahmed <saudahmed96x@gmail.com>
2023-11-19 00:36:10 -0500

    the searchbar is now working

--- clients/src/App.js
+++ clients/src/App.js
@@ -2,43 +2,44 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import token from "./data/token.json";
-// https://api.spotify.com/v1/recommendations/available-genre-seeds
+
 const BASE_URL = "https://api.spotify.com/v1/";
-const GENRE_END = "recommendations/available-genre-seeds";
+// const GENRE_END = "recommendations/available-genre-seeds";
 const rec_End = "recommendations";
 const search_End = "search";
 
 function App() {
-  const [genre, setGenre] = useState();
+  // const [genre, setGenre] = useState();
   const [searchItem, setSearchItem] = useState("photograph");
-  // const [songId, setSongId] = useState("");
   const [choices, setChoices] = useState([]);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, [formSubmitted]);
 
   function fetchData() {
-    axios
-      .get(`${BASE_URL}${GENRE_END}`, {
-        headers: { Authorization: `Bearer  ${token.access_token}` },
-      })
-      .then((res) => {
-        // console.log(res.data.genres);
-        setGenre(res.data.genres);
-      });
+    //below pulls list of potential genres
+    // axios
+    //   .get(`${BASE_URL}${GENRE_END}`, {
+    //     headers: { Authorization: `Bearer  ${token.access_token}` },
+    //   })
+    //   .then((res) => {
+    //     setGenre(res.data.genres);
+    //   });
 
     axios
       .get(`${BASE_URL}${search_End}?q=${searchItem}&type=track`, {
         headers: { Authorization: `Bearer  ${token.access_token}` },
       })
       .then((res) => {
-        console.log(res.data.tracks.items[0].id);
-
         fetchRec(res.data.tracks.items[0].id);
-        // setGenre(res.data.tracks.items);
       });
   }
 
-  function fetchRec(songNum) {
+  function fetchRec(songID) {
     axios
-      .get(`${BASE_URL}${rec_End}?seed_tracks=${songNum}`, {
+      .get(`${BASE_URL}${rec_End}?seed_tracks=${songID}`, {
         headers: { Authorization: `Bearer  ${token.access_token}` },
       })
       .then((res) => {
@@ -47,33 +48,60 @@ function App() {
       });
   }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  function formHandler(event) {
+    event.preventDefault();
+    console.log(searchItem);
+    setFormSubmitted(!formSubmitted);
+    console.log(formSubmitted);
+  }
+
+  const handleSearchBar = (event) => {
+    setSearchItem(event.target.value);
+  };
 
   return (
     <div className="App">
       <header className="nav">
-        <h1>SongFinder</h1>
-        <p>Find similar songs to your favourite song</p>
-        <div className="nav__searchbar">
-          <input
-            className="nav__search-input"
-            type="text"
-            placeholder="Search"
-          />
-        </div>
-        <button className="nav__button">Submit</button>
+        <h1 className="nav__title">SongFinder</h1>
       </header>
-      <ul>
-        {choices.map((choice) => {
-          return (
-            <>
-              <li>{choice.name}</li>
-            </>
-          );
-        })}
-      </ul>
+
+      <div className="main">
+        <p>Find similar songs to your favourite song</p>
+        <form className="main__form" onSubmit={formHandler}>
+          <div className="main__searchbar">
+            <input
+              className="main__search-input"
+              type="text"
+              placeholder="Search"
+              onChange={handleSearchBar}
+            />
+          </div>
+          <button className="main__button" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+
+      <div className="choices">
+        <ol className="choices__list">
+          {choices.map((choice, i) => {
+            if (i < 10) {
+              return (
+                <>
+                  <li key={choice.id} className="choices__item">
+                    <p id={choice.id} className="choices__song">
+                      <b>Song:</b> {choice.name},
+                    </p>
+                    <p id={choice.artists[0].id} className="choices__artist">
+                      <b>Artist:</b> {choice.artists[0].name}
+                    </p>
+                  </li>
+                </>
+              );
+            }
+          })}
+        </ol>
+      </div>
     </div>
   );
 }
